880d302ecbf9946ab454fc6d110222ac22d9db1a
nkkumawat <nk0kumawat@gmail.com>
2016-10-11 17:37:43 +0530

    h

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Narendra Kumawat</title>
+	<link rel="stylesheet" type="text/css" href="css/silde1.css">
+</head>
+<body >
+<div id="Slide1">
+	<div id="Name">HELLO,IT`S ME</div>
+	<div id="left"></div>
+	<div id="Name1">NARENDRA</div>
+	<div id="left1"></div>
+	<div id="developer">UI/UX DESIGNER - DEVELOPER</div>
+	<div id="readMore" onclick="Slide2()"><a href="#Slide2">Read More</a></div><div id="myProjects" onclick="Slide3()"><a href="#Slide3">My Projects</a></div>
+	<img src="images/menu.png" id="ImageMenu">
+</div>
+<!-- ===========slide 2================= -->
+<div id="Slide2">
+	<div id="ImageDiv"><img src="images/nk.jpg" id="Image"></div>
+	<div id="personal">
+		<p id="pera" ><strong id="perInfo">PERSONAL INFO.</strong><br><br><strong>NAME</strong><br><span>NARENDRA KUMAWAT</span><br><br><strong>E-MAIL</strong><br><span>NK0KUMAWAT@GMAIL.COM</span><br><br><strong>PHONE NO</strong><br><span>+91-9024444807</span><br><br><strong>WEBSITE</strong><br><span>WEBSITE.COM</span></p>
+	
+	</div>
+	<div id="details">
+	<p><STRONG id="perInfo">NARENDRA KUMAWAT</STRONG></p>
+	<p>TALENTED WEB DESIGNER / DEVELOPER</p>
+	<p>Enthusiastically reintermediate leveraged potentialities and wireless web services. Distinctively maintain ubiquitous best practices through market positioning materials. Progressively reintermediate ubiquitous channels via bricks-and-clicks technology. Distinctively formulate innovative relationships and high standards.</p><br><br>
+		<details class="details1">
+			<summary>WHO AM I?</summary>
+			<p>I AM A STUDENT OF NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA</p>
+		</details>
+		<details class="details1">
+			<summary>WHAT I'M REALLY GOOD AT?</summary>
+			<p>I AM A STUDENT OF NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA</p>
+		</details>
+		<details class="details1">
+			<summary>HOW CAN YOU CONTACT ME?</summary>
+			<p>I AM A STUDENT OF NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA</p>
+		</details>
+		<details class="details1">
+			<summary>WHAT IS MY HOBBIES</summary>
+			<p>I AM A STUDENT OF NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA</p>
+		</details>
+	</div>
+</div>
+<!-- slide 3==================== -->
+<div id="Slide3">
+	<div id="tech"><p><strong>TECHNICAL SKILLS</strong></p>
+Authoritatively foster bricks-and-clicks networks rather than superior architectures. Compellingly mesh frictionless imperatives vis-a-vis emerging.</div>
+<img src="images/I79.png">
+</div>
+
+
+
+<script type="text/javascript" src="js/slide.js"></script>
+</body>
+</html>
---
+++ js/slide.js
@@ -0,0 +1,13 @@
+
+function Slide2() {
+	// var slide=document.getElementById('left');
+	var url="#Slide2";
+	window.open(url,"_self");	
+}
+
+function Slide3() {
+	// var slide=document.getElementById('left');
+	var url="#Slide3";
+	window.open(url,"_self");	
+}
+
